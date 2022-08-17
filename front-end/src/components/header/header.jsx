086287dacd3631fa45ffe90e61971df468d9fb70
iftachbt <iftachbt@gmail.com>
2022-08-17 21:25:23 +0300

    change the heading to be material ui and dropdown menu for user

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -1,28 +1,173 @@
-import React from "react";
-import style from "./header.module.css"
-import {useNavigate} from "react-router-dom";
+import React,{useState} from "react";
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Container,
+    Button,
+    Tooltip,
+    MenuItem,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../actions/user";
+import { appBarTheme } from "./header.style";
+import { ThemeProvider } from '@mui/material/styles';
 
-function Header(props){
 
+const pages = [ 'About'];
+
+const Header = (props) => {
     const navigate = useNavigate();
-   
-    function handleClick(){
-        props.user ?navigate("/userProflie") :navigate("/login")
-    }
-    return(
-        <div className={style.main}>
-            <div className={style.tab}><p onClick={() => {navigate("/")}}>home page</p></div>
-            <div className={style.tab}><p onClick={() => {navigate(-1)}}>go back</p></div>
-            <div className={style.tab}><p>tab 2</p></div>
-            <div className={style.tab}><p>tab 3</p></div>
-            <div className={style.tab}><p>tab 4</p></div>
-            <div className={style.tab_hidden}></div>
-            <div className={style.tab_hidden}></div>
-            <div className={style.tab_hidden}></div>
-            <div className={style.tab_hidden}></div>
-            <div className={style.tab} onClick={handleClick}><p>{props.user ?"user" :"login"}</p></div>
-        </div>
-    )
+  const [anchorElNav, setAnchorElNav] = useState(null)
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  function logoutHandler(){
+    logout()
+    props.setUser(false)
+    navigate('/')
 }
 
+  return (
+    <ThemeProvider theme={appBarTheme} >
+    <AppBar position="static" color="primary">
+      <Container maxWidth="x5">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            HOME
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            HOME
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          {props.user ?(<Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="user">
+              <Button onClick={handleOpenUserMenu} sx={{ p: 1 }} color="inherit">USER</Button>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <p>{props.user.username}</p>
+                <MenuItem key='Logout' onClick={logoutHandler}>
+                  <Typography textAlign="center">Logout</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>) :(
+            <div>
+                <Button onClick={()=>navigate("/login")} color="inherit">Login</Button>
+            </div>
+            )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+    </ThemeProvider>
+  );
+};
 export default Header;
