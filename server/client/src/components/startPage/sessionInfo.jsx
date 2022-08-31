e0480dce0f137e2f355956f8dd3b982987f518a2
iftachbt <iftachbt@gmail.com>
2022-08-31 19:55:27 +0300

    adding style to session info

--- server/client/src/components/startPage/sessionInfo.jsx
+++ server/client/src/components/startPage/sessionInfo.jsx
@@ -5,12 +5,14 @@ import ProfileImage from "../profileImage/profileImage"
  function SessionInfo(props){
   return <div className={style.sessionInfo}>
             <div >
-            <h1>{props.res.name}</h1>
-            <ProfileImage 
-            race={props.race}
-            size="tiny"
-            />
-            <div>
+              <div className={style.mainInfo}>
+              <h1 className={style.infoH1}>{props.res.name}</h1>
+              <ProfileImage 
+              race={props.race}
+              size="tiny"
+              />
+            </div>
+            <div className={style.infoTop}>
                 <div >
                 level: {props.res.level}
                 </div>
@@ -18,19 +20,21 @@ import ProfileImage from "../profileImage/profileImage"
                 difficulty: {props.res.difficulty}
                 </div>
             </div>
-                <div>
-                ATK: {props.res.ATK}
-                </div>
-                <div>
-                HP: {props.res.HP}
-                </div>
-                <div>
-                shield: {props.res.shield}
-                </div>
-                <div>
-                gold: {props.res.gold}
-                </div>
-            <div>
+            <div className={style.infoMiddle}>
+              <div className={style.infoVal}>
+              ATK: {props.res.ATK}
+              </div>
+              <div className={style.infoVal}>
+              HP: {props.res.HP}
+              </div>
+              <div className={style.infoVal}>
+              shield: {props.res.shield}
+              </div>
+              <div className={style.infoVal}>
+              gold: {props.res.gold}
+              </div>
+            </div>
+            <div className={style.infoBottom}>
                 <div>
                 death: {props.res.deaths}
                 </div>
